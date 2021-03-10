a1b87fac40bb3bff073f8a604b5eec069537f217
rlnsy <rowlindsay@gmail.com>
2021-03-09 19:07:05 -0800

    extend logging capability

---
+++ services/http/api/lib/auth/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const logs = require('../logging');
+
+const config = JSON.parse(fs.readFileSync((path.join(__dirname, 'config.json'))));
+
+logs.info(`Config: ${JSON.stringify(config)}`);
+console.log('config: ', config)
--- services/http/api/lib/logging/index.js
+++ services/http/api/lib/logging/index.js
@@ -1,9 +1,11 @@
-function info(msg) {
-    console.log(`[INFO] ${msg}`);
+function info(...msg) {
+    console.log('[INFO]', ...msg);
 }
 
-function error(msg) {
-    console.log(`[ERROR] ${msg}`);
+function error(...msg) {
+    console.log('[ERROR]', ...msg);
 }
+
+info('Logging enabled')
 
 module.exports = { info, error };
