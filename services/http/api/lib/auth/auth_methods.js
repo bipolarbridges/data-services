9d4a20eb4255ac115f6f742e8bef88b73438547d
rlnsy <rowlindsay@gmail.com>
2021-03-11 12:35:15 -0800

    set up call to remote auth

--- services/http/api/lib/auth/auth_methods.js
+++ services/http/api/lib/auth/auth_methods.js
@@ -2,13 +2,45 @@ const fs = require('fs');
 const path = require('path');
 const logs = require('../logging');
 const crypt = require('crypto');
+const axios = require('axios');
+const { InternalError } = require('../errors');
 
-const config = JSON.parse(fs.readFileSync((path.join(__dirname, 'config.json'))));
-logs.info('Config: ', config);
+class AuthError extends InternalError {};
 
 function basicHash(sec) {
     return crypt.createHash('sha256')
         .update(sec).digest('hex');
+}
+
+let config = JSON.parse(fs.readFileSync((path.join(__dirname, 'config.json'))));
+config = config['auth_server'];
+logs.info('Config: ', config);
+
+const remote = axios.create({
+	baseURL: `${config.protocol}://${config.addr}:${config.port}`
+});
+
+async function getRemoteId(token) {
+    try {
+        const res = await remote.post('/validate', { token }, {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        if (res.status != 200) {
+            throw new Error("Wrong status code response from remote auth");
+        } else if (!res.data['result']) {
+            return null;
+        } else {
+            if (!res.data['id']) {
+                throw new Error("No id from remote auth");
+            } else {
+                return res.data['id']
+            }
+        }
+    } catch (e) {
+        throw new AuthError(e);
+    }
 }
 
 module.exports = {
@@ -28,5 +60,14 @@ module.exports = {
             return results.records.length > 0;
         },
         // TODO: add id token option
+        (req, auth) => async (_) => {
+            const id = await getRemoteId(auth);
+            if (!id) {
+                return false;
+            } else {
+                logs.info('Authenticated: ', id);
+                return true; // TODO
+            }
+        }
     ]
 }
