0d40c832c0e75260846b168b17e02f07076fa5f9
rlnsy <rowlindsay@gmail.com>
2021-03-10 23:57:05 -0800

    add warn level to logging

--- services/http/api/lib/logging/index.js
+++ services/http/api/lib/logging/index.js
@@ -2,14 +2,19 @@ function info(...msg) {
     console.log('[INFO]', ...msg);
 }
 
+function warn(...msg) {
+    console.log('[WARNING]', ...msg);
+}
+
 function error(...msg) {
     console.log('[ERROR]', ...msg);
 }
+
 function debug(...msg) {
     console.log('[DEBUG]', ...msg);
 }
 
 info('Logging enabled')
 
-module.exports = { info, error, debug };
+module.exports = { info, warn, error, debug };
 
