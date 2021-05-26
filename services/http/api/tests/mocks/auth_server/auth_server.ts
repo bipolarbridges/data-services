72a476c9e1429ca18013e3643fd95cdd6b19cf98
kaede <kaede.ito2001@gmail.com>
2021-05-26 12:50:21 -0700

    updated dependencies to build

--- services/http/api/tests/mocks/auth_server/auth_server.ts
+++ services/http/api/tests/mocks/auth_server/auth_server.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import { json } from 'body-parser';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
-app.use(json());
+app.use(express.json());
 
 type Data = {
     [key:string]: string
