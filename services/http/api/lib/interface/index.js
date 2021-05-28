50c0fad4374d9e44c4c06a9ff67ff659358072c3
kaede <kaede.ito2001@gmail.com>
2021-05-28 15:53:33 -0700

    added helper function for date format

--- services/http/api/lib/interface/index.js
+++ services/http/api/lib/interface/index.js
@@ -17,11 +17,7 @@ module.exports = {
     createMeasurement: (m) => async (db) => {
         // decoding the timestamp per the format of this post:
         // https://stackoverflow.com/a/847196
-        const date = new Date(m.date * 1000)
-        const year = date.getFullYear()
-        const month = date.getMonth()
-        const day = date.getDate()
-        const time = 3600*date.getHours() + 60*date.getMinutes() + date.getSeconds()
+        const date = dateTransformer(m.date);
         const user = await db.run(
             "MATCH (u:User{uid:$uid}) "                                                     +
             "MERGE (m:Measurement{type: $type, value: $value}) "                            +
@@ -30,16 +26,48 @@ module.exports = {
             "MERGE (d) -[:Includes]-> (m) "                                                 +
             "MERGE (u) -[:Recorded]-> (m) "                                                 +
             "RETURN u;", 
-            {...m, time, day, month, year })
+            {...m, time: date.time, day: date.day, month: date.month, year: date.year })
         return user.records.length > 0
     },
 
     createSurvey: (s) => async (db) => {
-        const date = new Date(s.date * 1000)
-        const year = date.getFullYear()
-        const month = date.getMonth()
-        const day = date.getDate()
-        const time = 3600*date.getHours() + 60*date.getMinutes() + date.getSeconds()
-        const user = await db.run("", {...s, time, day, month, year })
+        const date = dateTransformer(s.date);
+        const user = await db.run("", {...s, time: date.time, day: date.day, month: date.month, year: date.year })
+    }
+}
+
+/* 
+type MDate = {
+    time: number,
+    year: number,
+    month: number,
+    day: number,
+} */
+
+// decoding the timestamp per the format of this post:
+// https://stackoverflow.com/a/847196
+/**
+ * 
+ * @typedef tDate
+ * @property {number} year
+ * @property {number} month 
+ * @property {number} day 
+ * @property {number} time
+ * 
+ * @param {number} input 
+ * @returns {tDate}
+ */
+function dateTransformer(input) {
+    const date = new Date(input * 1000);
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+    const time = 3600 * date.getHours() + 60 * date.getMinutes() + date.getSeconds();
+    
+    return {
+        year, 
+        month,
+        day, 
+        time
     }
 }
