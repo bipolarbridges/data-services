2089bb1caa72c2c308f0bddb0b148126507513bc
kaede <kaede.ito2001@gmail.com>
2021-06-16 01:29:57 -0700

    skip spec.ts files

--- services/http/api/jest.config.js
+++ services/http/api/jest.config.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  preset: 'ts-jest',
+  testPathIgnorePatterns: ["/node_modules/", ".spec.ts"],
   testEnvironment: 'node',
+  "verbose": false
 };
