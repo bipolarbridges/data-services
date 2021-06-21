84c73bd90144d0600646890547a7d03539ecce2b
rlnsy <rowlindsay@gmail.com>
2021-06-21 00:05:47 -0700

    small refactor:

---
+++ services/http/api/lib/fixtures/FixtureLoader.ts
@@ -0,0 +1,27 @@
+import { Database, database, DatabaseProcedure } from '../db';
+import { error, info } from '../logging';
+
+export type Fixture = DatabaseProcedure<void>;
+
+export default class FixtureLoader {
+
+    private db: Database;
+
+    constructor() {
+            this.db = database();
+    }
+
+    async load(fix: Fixture) {
+        info('Clearing out old data...');
+        await this.db.run('MATCH (n) DETACH DELETE n;');
+        info('Done clearing data.');
+        info('Loading fixture data...');
+        try {
+            await this.db.exec(fix);
+        } catch (err) {
+            throw new Error(err);
+        }
+        info('Loaded data.');
+    }
+
+}
--- services/http/api/lib/fixtures/examples.ts
+++ services/http/api/lib/fixtures/examples.ts
@@ -4,28 +4,8 @@ import { error, info } from '../logging';
 import { Session } from 'neo4j-driver';
 import dotenv from 'dotenv';
 import { makeMeasurementProperties, mergedMeasurement } from '../interface';
+import FixtureLoader from './FixtureLoader';
 dotenv.config();
-
-async function main() {
-    const db: Database = database();
-    info('Clearing out old data...');
-    await db.run('MATCH (n) DETACH DELETE n;');
-    info('Done clearing data.');
-    info('Beginning to load example data...');
-    try {
-        await Promise.all([
-            (await createClients(db)), 
-        ]);
-        
-    } catch (err) {
-        error(err);
-        process.exit(1);
-    }
-    info('Loaded example data.');
-    process.exit(0);
-}
-
-main();
 
 function createMergeProperties(name: string, source: string) {
     return {
@@ -52,24 +32,22 @@ function createMergeProperties(name: string, source: string) {
     };
 }
 
-async function createClients(db: Database) {
-    const users = ['1', '2']
-    type measurementList = {
-        [key: string] : mergedMeasurement[]
-    };
+const users = ['1', '2']
+type measurementList = {
+    [key: string] : mergedMeasurement[]
+};
 
-    const measurements: measurementList = {
-        [`${users[0]}`]: [
-            makeMeasurementProperties(90, 8, 13, 9, 2021, 1890237291, undefined, createMergeProperties('heartrate', 'measurement')),
-            makeMeasurementProperties(0.8, 3, 1, 9, 2021, 18966712291, undefined, createMergeProperties('sentiment', 'measurement')),
-        ],
-        [`${users[1]}`]: [
-            makeMeasurementProperties(6, 12, 27, 3, 2021, 189667111111, undefined, createMergeProperties('sentiment', 'measurement')),
-        ],
-    };
+const measurements: measurementList = {
+    [`${users[0]}`]: [
+        makeMeasurementProperties(90, 8, 13, 9, 2021, 1890237291, undefined, createMergeProperties('heartrate', 'measurement')),
+        makeMeasurementProperties(0.8, 3, 1, 9, 2021, 18966712291, undefined, createMergeProperties('sentiment', 'measurement')),
+    ],
+    [`${users[1]}`]: [
+        makeMeasurementProperties(6, 12, 27, 3, 2021, 189667111111, undefined, createMergeProperties('sentiment', 'measurement')),
+    ],
+};
 
-    const creator = async (session: Session, models: allModels): Promise<boolean> => { 
-        
+new FixtureLoader().load( async (session: Session, models: allModels) => { 
         const createdList = users.map(uid => {
             return {
                 uid,
@@ -95,17 +73,12 @@ async function createClients(db: Database) {
             }
         });
 
-        try {
-            await models.user.createMany(createdList, { session, merge: true });
-            return true;
-        } catch (err) {
-            throw new Error(err);
-        }
-    }
-
-    try {
-        return db.exec(creator);
-    } catch (err) {
-        throw new Error(err);
-    }
-}
+        await models.user.createMany(createdList, { session, merge: true });
+    })
+.then(() => {
+	process.exit(0);
+})
+.catch((err) => {
+	error(err);
+	process.exit(1);
+});
