38e0bd3b9f241699aec2fda58f43175b3b49ed1a
kaede <kaede.ito2001@gmail.com>
2021-07-05 17:36:15 -0700

    typing fix

--- services/http/api/lib/auth/index.ts
+++ services/http/api/lib/auth/index.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { authMethods } from './auth_methods';
+import { AuthMethod, authMethods } from './auth_methods';
 import findOne from '../util/misc';
 import { warn } from '../logging';
 import { Database } from '../db';
@@ -8,7 +8,7 @@ export default function auth(db: Database) {
   return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const provided = req.get('Authorization');
     if (!provided
-            || !(await findOne(authMethods, async (m) => db.exec(m(req, provided))))) {
+            || !(await findOne(authMethods, async (m: AuthMethod) => db.exec(m(req, provided))))) {
       warn('Unauthorized request attempted');
       res.status(403).send({
         message: 'Not authorized',
--- services/http/api/lib/util/misc.ts
+++ services/http/api/lib/util/misc.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line import/prefer-default-export
-export async function findOne<T>(a: T[], cond: (a: T) => Promise<boolean>): Promise<T> {
+export default async function findOne<T>(a: T[], cond: (a: T) => Promise<boolean>): Promise<T> {
   let i = 0;
   while (i < a.length) {
     if (await cond(a[i])) {
