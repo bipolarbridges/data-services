7cc5ed3bab21d0e8011037a70655e2ad80835a51
kaede <kaede.ito2001@gmail.com>
2021-07-23 00:32:33 -0700

    formatting for eslintrc

--- services/http/api/.eslintrc.js
+++ services/http/api/.eslintrc.js
@@ -32,7 +32,7 @@ module.exports = {
         "import/resolver": {
             "typescript": {
                 "alwaysTryTypes": true // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
-              },
+            },
         },
         "typescript": {
             "directory": "packages/*/tsconfig.json"
